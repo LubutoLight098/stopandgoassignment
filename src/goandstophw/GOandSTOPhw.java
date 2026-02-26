181001a04f55b751177f5e2c180090cd6cd67eec
unknown <st10515464@myemeris.edu.za>
2026-02-26 08:55:08 +0200

    final commit

---
+++ src/goandstophw/GOandSTOPhw.java
@@ -0,0 +1,40 @@
+/*
+ *fkhjs
+ *
+ */
+package goandstophw;
+
+/**
+ ** @author Alichi
+ */
+public class GOandSTOPhw {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    /**
+     * 
+     * my first app about go and stop
+     */
+
+        System.out.println("                                         __ __ ___  __ __ __ __ __ __ __ __ __ __ __ __ __ __ __");
+    System.out.println();
+    System.out.println("                         ********                                                                         XXXXXXXX                             ");
+    System.out.println("                      *           *                                                                   X               X                                  ");
+    System.out.println("                    *     S T O P   *                                                               X        GO          X                                                      ");
+    System.out.println("                      *           *                                                                   X               X          ");
+    System.out.println("                         ********                                                                         XXXXXXXX                                                    ");
+    System.out.println("                            **                                                                               XX                                          ");
+    System.out.println("                            **                                                                               XX                                                  ");
+    System.out.println("                            **                                                                               XX                                                              ");
+    System.out.println("                            **                                                                               XX                        ");
+    System.out.println("                           ****                                                                             XXXX                                                        ");
+    System.out.println("                        **********                                                                       XXXXXXXXXX                                      ");
+    System.out.println("                                                                                                                                                         ");
+    System.out.println("|--------------------------------------------------------------------------------------------------------------------------------------------------------------| ");
+    System.out.println("|--------------------------------------------------------------------------------------------------------------------------------------------------------------|");
+    
+    }
+    
+}
